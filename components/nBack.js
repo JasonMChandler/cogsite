8ca454c3630ee84fd1b53abb4c7b904d0d26441e
Jason Chandler <jasonmichaelchandler@gmail.com>
2021-08-18 05:43:01 +0000

    adding sequence generator

--- components/nBack.js
+++ components/nBack.js
@@ -6,30 +6,46 @@ export default function nBack(props) {
     const dispatch = useDispatch();
     const state = useSelector((state) => state.nback)
     useEffect(() => {
-        console.log("useEffect")
-        if (gridItem.current) {
+
+
 
             dispatch({ type: "NBACK", payload: { key: "width", value: window.innerWidth } })
             dispatch({ type: "NBACK", payload: { key: "height", value: window.innerHeight } })
             dispatch({ type: "NBACK", payload: { key: "largerDimension", value: window.innerHeight > window.innerWidth ? "height" : "width" } })
-        }
-    }, [state.size])
 
 
-    return (<Grid style={{width:(() => {return state.largerDimension === "height" ? state.width : state.height})(), height:(() => {return state.largerDimension === "height" ? state.width : state.height})()}} container><Grid onClick={() => dispatch({type:"NBACK", payload:{key:"size",value:state.size+1}})} ref={gridItem} justifyContent="space-around" alignItems="center" container>
+
+            dispatch({ type: "NBACK", payload: { key: "sequence", value: state.generateSequence(32,3,.40)} })
+
+
+
+            let timer = setTimeout(function(){
+            	dispatch({ type: "NBACK", payload: { key: "timer", value: state.timer+1 } })
+            	dispatch({ type: "NBACK", payload: { key: "time", value: Date.now()} })
+
+            },10)
+
+            return () => {
+        	clearTimeout(timer);
+      		};
+		
+    }, [])
+
+
+    return (<Grid style={{width:(() => {return state.largerDimension === "height" ? state.width : state.height})(), height:(() => {return state.largerDimension === "height" ? state.width : state.height})()}} container><Grid onClick={() => dispatch({type:"NBACK", payload:{key:"size",value:state.size+1}})} justifyContent="space-around" alignItems="center" container>
     	<Grid item><Paper style={{width:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})(),height:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})()}} square={true}></Paper></Grid>
     	<Grid item><Paper style={{width:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})(),height:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})()}} square={true}></Paper></Grid>
     	<Grid item><Paper style={{width:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})(),height:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})()}} square={true}></Paper></Grid>
     	</Grid>
 
-    	<Grid onClick={() => dispatch({type:"NBACK", payload:{key:"size",value:state.size+1}})} ref={gridItem} justifyContent="space-around" alignItems="center" container>
+    	<Grid onClick={() => dispatch({type:"NBACK", payload:{key:"size",value:state.size+1}})} justifyContent="space-around" alignItems="center" container>
     	<Grid item><Paper style={{width:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})(),height:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})()}} square={true}></Paper></Grid>
     	<Grid item><Paper style={{width:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})(),height:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})()}} square={true}></Paper></Grid>
     	<Grid item><Paper style={{width:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})(),height:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})()}} square={true}></Paper></Grid>
     	</Grid>
 
 
-    	<Grid onClick={() => dispatch({type:"NBACK", payload:{key:"size",value:state.size+1}})} ref={gridItem} justifyContent="space-around" alignItems="center" container>
+    	<Grid onClick={() => dispatch({type:"NBACK", payload:{key:"size",value:state.size+1}})} justifyContent="space-around" alignItems="center" container>
     	<Grid item><Paper style={{width:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})(),height:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})()}} square={true}></Paper></Grid>
     	<Grid item><Paper style={{width:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})(),height:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})()}} square={true}></Paper></Grid>
     	<Grid item><Paper style={{width:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})(),height:(() => {return state.largerDimension === "height" ? (state.width - 30)/3 : (state.height - 30)/3})()}} square={true}></Paper></Grid>
--- reducers/index.js
+++ reducers/index.js
@@ -1,17 +1,85 @@
 const initialState = {
-  nback: {size:0},
+  nback: {
+  	size:0,
+  	timer:0,
+  	time:0,
+  	
+  	generateSequence: function(length,nback, percentageOfHits){
+  		var targetNumber = Math.floor(length*percentageOfHits);
+  		var finalArray;
+  		//gives you a random number between the min and max, inclusive
+  		function randomIntFromInterval(min, max) { // min and max included 
+  		return Math.floor(Math.random() * (max - min + 1) + min)
+		}
+			var seqArray = [];
+
+			for (var i = 0; i< length; i++) {
+				seqArray.push(randomIntFromInterval(0,9));
+			}
+		// CheckHitNumber checks how many matches are in an array once supplied with an array of numbers and the n back number
+		function checkHitNumber (arr,nback) {
+			var checkArray = [...arr];
+			var numOfHits = 0;
+			for (var i = 0; i< checkArray.length-nback; i++) {
+				if (checkArray[i] === checkArray[i+nback] && Number.isInteger(checkArray[i])) {
+
+					numOfHits++;
+				}
+			}
+
+			return numOfHits;
+		}
+
+		//fillArray adds nback hits in random spots until it reaches a certain amount
+		function fillArray(arr,nback) {
+
+			var length = arr.length;
+			var seqArrayFill = [...arr];
+			var numOfHits = 0;
+			var seqSpot = randomIntFromInterval(0,seqArrayFill.length-nback-1);
+			var seqItem = randomIntFromInterval(0,9);
+
+
+			if (checkHitNumber(seqArrayFill,nback) < targetNumber) {
+				// if there aren't enough hits, add more					
+				seqArrayFill[seqSpot] = seqItem;
+				seqArrayFill[seqSpot+nback] = seqItem;
+
+				return fillArray(seqArrayFill,nback);
+
+			} else if (checkHitNumber(seqArrayFill,nback) > targetNumber) {
+				//if there are too many hits random change the sequence
+				seqArrayFill[seqSpot+nback] = seqItem;
+				return fillArray(seqArrayFill,nback);
+			} else {
+				// if just right return the array
+				return seqArrayFill;
+
+			}
+
+
+		}
+		finalArray = fillArray(seqArray, nback);
+
+
+  		return seqArray;
+  	},
+  	sequence:[],
+  },
 };
-//action.payload
-//[state.articles[state.articles.length].title]
+
 const rootReducer = (state = initialState, action) => {
 	console.log(action.type);
   switch (action.type) {
 
     case "CLICK":
-    console.log("CLICK",action.type);
       return {...state};
     case "NBACK":
-    console.log("NBACK",action.payload);
+
+
+    	//add state with payload - payload.key tells what state object key to change - value is the value to be set for that state object key
+
+
       return {...state, nback:{...state.nback, [action.payload.key]:action.payload.value}};
     default:
     console.log(action.type);
